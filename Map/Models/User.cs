cf32837f29baae81aebfc7e7ca7169594c21ebe8
Gintaras Grigaliunas <gintaras.grigaliunas@aon.lt>
2017-07-17 13:50:16 +0300

    pakeista

---
+++ Map/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Map.Models
+{
+    public class User
+            {
+        [Key]
+
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string lat { get; set; }
+        public string lng { get; set; }
+    }
+}
